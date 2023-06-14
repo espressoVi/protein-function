Fixed IA, removed mixed precision training

train_test.py
@@ -8,12 +8,15 @@ from tqdm import trange,tqdm
 config_dict = toml.load("config.toml")
 train_param = config_dict['train']
 
-def train(model, device, train_dataset, val_dataset, metrics):
+def train(model, device, dataset, metrics):
+    train_dataset, val_dataset = dataset.get_train_dataset()
+    infer = dataset.fill
+    """ train_dataset, val_dataset are torch TensorDatasets
+    which contains (input_ids, attention_masks, labels). """
     train_dataloader = DataLoader(train_dataset, batch_size = train_param['MINI_BATCH_SIZE'], shuffle = True, )
     optimizer_parameters = [param for name, param in model.named_parameters() if 'protbert' not in name]
     #optimizer_parameters = model.parameters()
     optimizer = AdamW(optimizer_parameters, lr = train_param['LR'], eps = 1e-8, weight_decay = 1e-4)
-    scaler = torch.cuda.amp.GradScaler()
     epochs = train_param['EPOCHS']
     counter, train_loss, loss = 1, 0.0,0.0
     model.zero_grad()
@@ -21,26 +24,23 @@ def train(model, device, train_dataset, val_dataset, metrics):
         epoch_iterator = tqdm(train_dataloader, desc="Iteration")
         for i,batch in enumerate(epoch_iterator):
             model.train()
-            with torch.autocast(device_type="cuda",dtype = torch.float16):
-                batch = tuple(t.to(device) for t in batch)
-                inputs = { "input_ids": batch[0],
-                          "attention_masks": batch[1],
-                          "labels":batch[2],}
-                loss, predicts = model(**inputs)
+            batch = tuple(t.to(device) for t in batch)
+            inputs = { "input_ids": batch[0],
+                      "attention_masks": batch[1],
+                      "labels":batch[2],}
+            loss, predicts = model(**inputs)
             train_loss += loss.item()
-            scaler.scale(loss).backward()
+            loss.backward()
             if (i+1)%train_param['ACCUMULATE'] == 0:
-                scaler.step(optimizer)
-                #scaler.step(scheduler)
-                scaler.update()
+                optimizer.step()
                 model.zero_grad()
             counter += 1
             epoch_iterator.set_description("Loss :%f" % (train_loss/counter))
             epoch_iterator.refresh()
-        evaluate(model, device, val_dataset, metrics)
+        evaluate(model, device, val_dataset, infer, metrics)
 
-def evaluate(model, device, dataset, metrics):
-    eval_dataloader = DataLoader(dataset, batch_size = train_param['TEST_BATCH_SIZE'], shuffle = False, )
+def evaluate(model, device, val_dataset, infer, metrics):
+    eval_dataloader = DataLoader(val_dataset, batch_size = train_param['TEST_BATCH_SIZE'], shuffle = False, )
     F1 = metrics.metrics['CAFAMetric']
     preds, labels = [],[]
     model.eval()
@@ -55,14 +55,14 @@ def evaluate(model, device, dataset, metrics):
         preds.extend(pred_batch)
         labels.extend(label_batch)
     preds,labels = np.array(preds),np.array(labels)
-    threshold = find_threshold(F1, labels, preds)
-    outputs = (preds>threshold).astype(int)
+    threshold = find_threshold(F1, labels, preds, infer)
+    outputs = infer((preds>threshold).astype(int))
     print(metrics.eval_and_show(labels, outputs))
 
-def find_threshold(metric, labels, preds):
+def find_threshold(metric, labels, preds, infer):
     best = 0
-    for i in tqdm(np.linspace(0.1,0.99,100), desc="Tuning threshold"):
-        score = metric(labels, (preds>i).astype(int))
+    for i in tqdm(np.linspace(0.1,np.amax(preds),100), desc="Tuning threshold"):
+        score = metric(labels, infer((preds>i).astype(int)))
         if score > best:
             best = score 
             threshold = i

utils/dataset.py
@@ -14,7 +14,7 @@ files = config_dict['files']
 
 class Preprocess:
     def __init__(self, subgraph = None):
-        assert subgraph in config_dict['dataset']['SUB_GRAPHS'] or subgraph is None
+        assert subgraph in config_dict['dataset']['SUB_GRAPHS']
         self._train_labels = self._load_labels()
         self.label_graph = self._graph_prune(subgraph,self._train_labels,)
         self.class_number = len(self.label_graph)
@@ -113,8 +113,8 @@ class Dataset:
     def get_train_dataset(self):
         _, proteins, labels = self.dataset.get_dataset('train')
         # DELETE FOR ACTUAL RUN
-        proteins = proteins[:1000]
-        labels = labels[:1000]
+        #proteins = proteins[:1000]
+        #labels = labels[:1000]
         # UPTO HERE
         proteins = self.tokenize(proteins)
         split = self.get_folds(labels)
@@ -137,6 +137,17 @@ class Dataset:
                           padding = "max_length", truncation = True, return_tensors = 'pt',)
         print(f"Tokenization took {perf_counter() - _time_start:.2f}s")
         return sequences
+    def fill(self, predictions):
+        rv = []
+        for pred in predictions:
+            prediction = np.zeros_like(pred, dtype = np.bool)
+            for idx in np.where(pred == 1)[0]:
+                anc = np.array([self.dataset.go2idx[i] for i in nx.ancestors(self.dataset.label_graph,
+                             self.dataset.idx2go[idx])]).astype(int)
+                prediction[anc] = 1
+                prediction[idx] = 1
+            rv.append(prediction)
+        return np.array(rv)
     @staticmethod
     def get_folds(labels):
         return np.where(IterativeStratification(labels, n_splits=config_dict['dataset']['N_FOLDS']) == 0, True, False)

utils/metric.py
@@ -63,7 +63,7 @@ class Metrics:
         wn_gt = (labels*self.ia_weights).sum(axis=1)
         wn_pred = (outputs*self.ia_weights).sum(axis=1)
         wn_intersection = (intersection*self.ia_weights).sum(axis=1)
-        pr = np.where(n_pred>0, wn_intersection/wn_pred, 0).sum()/(np.where(n_pred>0,1,0).sum())
+        pr = np.where(wn_pred>0, wn_intersection/wn_pred, 0).sum()/(np.where(n_pred>0,1,0).sum())
         rc = np.where(n_gt>0,wn_intersection/wn_gt,0).sum()/(np.where(n_gt>0,1,0).sum())
         n = 2*pr*rc
         d = pr+rc